ab4c3ab3e705c8c5b8878c84b356de6db41267d6
rrrongon <rubayetrongon@gmail.com>
2016-04-02 17:50:21 +0600

    navigation tab with pager

---
+++ app/src/main/java/com/test/iptv/materialdesignone/FirstTabFrag.java
@@ -0,0 +1,27 @@
+package com.test.iptv.materialdesignone;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Developed by root on 4/2/16.
+ */
+public class FirstTabFrag extends Fragment {
+    public static FirstTabFrag getInstance(){
+        FirstTabFrag firstTabFragIns = new FirstTabFrag();
+        return firstTabFragIns;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        View layout = inflater.inflate(R.layout.firsttabcontent,container,false);
+        return layout;
+    }
+}
--- app/src/main/java/com/test/iptv/materialdesignone/MainActivity.java
+++ app/src/main/java/com/test/iptv/materialdesignone/MainActivity.java
@@ -3,16 +3,26 @@ package com.test.iptv.materialdesignone;
 import android.app.ActionBar;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+
+import com.test.iptv.tabs.SlidingTabLayout;
 
 public class MainActivity extends ActionBarActivity {
 
     Toolbar toolbar;
+    private ViewPager mPager;
+    private SlidingTabLayout mTab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,11 @@ public class MainActivity extends ActionBarActivity {
         DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
 
         drawerFrag.setUp(drawerLayout,toolbar);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        mTab = (SlidingTabLayout) findViewById(R.id.tabs);
+        mTab.setViewPager(mPager);
     }
 
     @Override
@@ -50,5 +65,38 @@ public class MainActivity extends ActionBarActivity {
             Toast.makeText(getApplicationContext(),"Navigate",Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    class MyPagerAdapter extends FragmentPagerAdapter{
+
+        String[] tabs;
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+            tabs = getResources().getStringArray(R.array.tabs);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return  tabs[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if(position==1){
+                FirstTabFrag firstTabFrag = FirstTabFrag.getInstance();
+                return firstTabFrag;
+            }
+            else {
+                SecondTabFrag secondTabFrag = SecondTabFrag.getInstance();
+                return secondTabFrag;
+            }
+
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
     }
 }
